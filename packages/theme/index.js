d4587a4f5e90f3dc1f9b71cd2bde27de1f8d389a
Paul Mason <paul.mason@outlook.com>
2022-01-03 11:39:36 +0200

    Update

--- packages/theme/index.js
+++
@@ -1,9 +0,0 @@
-/**
-@license
-MIT License
-Copyright (c) 2021 Paul H Mason. All rights reserved.
-*/
-//export { Theme, ThemeMode, DesignToken, Icon } from './lib/Theme.js';
-export { Theme } from './lib/Theme.js';
-export { themeManager } from './lib/ThemeManager.js';
-//# sourceMappingURL=index.js.map
